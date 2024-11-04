b06e023bb544e4e6351ae3417fff2bd24b34e3be
hpp2334 <hpp2334@outlook.com>
2024-11-05 02:43:41 +0800

    fix: stacktrace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
     }
     compileOptions {
