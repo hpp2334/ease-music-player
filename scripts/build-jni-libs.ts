82ebc6ec747385b1f6d5fd30f30a81bb9e286e65
hpp2334 <hpp2334@outlook.com>
2026-04-16 22:21:34 +0800

    fix: generate UNIFFI bindings into commonMain only to avoid redeclarations

--- scripts/build-jni-libs.ts
+++ scripts/build-jni-libs.ts
@@ -1,7 +1,6 @@
 import { execSync } from "node:child_process";
-import { BUILD_GRADLE_KTS, ENVS, ROOT, RUST_LIBS_ROOTS, TARGETS } from "./base";
+import { ROOT, RUST_LIBS_ROOTS, TARGETS } from "./base";
 import path from "node:path";
-import { readFileSync } from "node:fs";
 
 console.log("Build ease-client in debug mode");
 execSync(`cargo build -p ease-client-backend`, {
@@ -10,20 +9,6 @@ execSync(`cargo build -p ease-client-backend`, {
 });
 
 for (const buildTarget of TARGETS) {
-  console.log(`Generate kotlin file of ${buildTarget}`);
-  execSync(
-    `cargo run -p ease-client-android-ffi-builder generate --library ${path.resolve(RUST_LIBS_ROOTS, "./target/debug/libease_client_backend.so")} --language kotlin --out-dir ${path.resolve(ROOT, "shared/src/androidMain/kotlin/")}`,
-    {
-      stdio: "inherit",
-      cwd: RUST_LIBS_ROOTS,
-      env: {
-        ...process.env,
-        RUST_BACKTRACE: "1",
-        CARGO_NDK_ANDROID_PLATFORM: "34",
-      },
-    },
-  );
-
   console.log(`Generate jniLibs of ${buildTarget}`);
   execSync(
     `cargo ndk --no-strip --target ${buildTarget} -o ${path.resolve(ROOT, "androidApp/src/main/jniLibs")} build --release --lib`,
@@ -39,9 +24,9 @@ for (const buildTarget of TARGETS) {
   );
 }
 
-console.log("Generate kotlin bindings for desktop");
+console.log("Generate kotlin bindings");
 execSync(
-  `cargo run -p ease-client-android-ffi-builder generate --library ${path.resolve(RUST_LIBS_ROOTS, "./target/debug/libease_client_backend.so")} --language kotlin --out-dir ${path.resolve(ROOT, "shared/src/desktopMain/kotlin/")}`,
+  `cargo run -p ease-client-android-ffi-builder generate --library ${path.resolve(RUST_LIBS_ROOTS, "./target/debug/libease_client_backend.so")} --language kotlin --out-dir ${path.resolve(ROOT, "shared/src/commonMain/kotlin/")}`,
   {
     stdio: "inherit",
     cwd: RUST_LIBS_ROOTS,
